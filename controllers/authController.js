5f0b45aa224b92fe076b2836fb785b5bb7ec183e
Aman Swarnakar <80145855+amanswarnakar@users.noreply.github.com>
2022-03-21 11:18:34 +0530

    Fix login error where it says incorrect password
    Removed the { expiresIn: maxAge } property of the JWT.
    Now the token does not become invalid.

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,22 +22,15 @@ const handleErrors = (err) => {
   }
   // validation errors
   if (err.message.includes("user validation failed")) {
-    // console.log(err);
     Object.values(err.errors).forEach(({ properties }) => {
-      // console.log(val);
-      // console.log(properties);
       errors[properties.path] = properties.message;
     });
   }
   return errors;
 };
 
-const maxAge = 3 * 24 * 60 * 60;
-
 const createToken = (id) => {
-  return jwt.sign({ id }, process.env.SECRET, {
-    expiresIn: maxAge * 1000,
-  });
+  return jwt.sign({ id }, process.env.SECRET);
 };
 
 module.exports.signup_get = (req, res) => {
@@ -72,7 +65,7 @@ module.exports.signup_post = async (req, res) => {
       password,
     });
     const token = createToken(user._id);
-    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.cookie("jwt", token, { httpOnly: true });
     res.status(201).json({ user: user._id, username: user.username });
   } catch (err) {
     const errors = handleErrors(err);
@@ -85,7 +78,7 @@ module.exports.login_post = async (req, res) => {
   try {
     const user = await User.login(email, password);
     const token = createToken(user._id);
-    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.cookie("jwt", token, { httpOnly: true });
 
     res.status(200).json({ user: user._id, username: user.username });
   } catch (err) {
